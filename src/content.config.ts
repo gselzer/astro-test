194143ffef05f0b2b26d67dd229d3de91ad04a6d
Gabriel Selzer <gjselzer@wisc.edu>
2025-09-30 23:58:16 -0500

    Various changes related to blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,10 @@
 // Import the glob loader
-import { glob } from "astro/loaders";
+import { glob, file } from "astro/loaders";
 // Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // Define a `loader` and `schema` for each collection
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/blog" }),
     schema: z.object({
       title: z.string(),
       pubDate: z.date(),
@@ -13,9 +13,32 @@ const blog = defineCollection({
       image: z.object({
         url: z.string(),
         alt: z.string()
-      }),
+      }).optional(),
+      icon: z.object({
+        url: z.string(),
+        alt: z.string()
+      }).optional(),
       tags: z.array(z.string())
     })
 });
+const external = defineCollection({
+    loader: file('src/content/external.yaml'),
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }).optional(),
+      icon: z.object({
+        url: z.string(),
+        alt: z.string()
+      }).optional(),
+      tags: z.array(z.string()),
+      redirect: z.string()
+    })
+});
 // Export a single `collections` object to register your collection(s)
-export const collections = { blog };
+export const collections = { blog, external};
